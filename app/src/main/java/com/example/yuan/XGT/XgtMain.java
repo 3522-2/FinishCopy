42c8528ea54d4fa58e1cb39d8a357dc92420adb0
1623147254 <1623147254@users.noreply.github.com>
2018-12-11 12:21:22 +0800

    12:11效果图界面

---
+++ app/src/main/java/com/example/yuan/XGT/XgtMain.java
@@ -0,0 +1,15 @@
+package com.example.yuan.XGT;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yuan.R;
+
+public class XgtMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_xgt_main);
+    }
+}
